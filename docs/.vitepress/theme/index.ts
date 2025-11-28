8f1bc8cab3dde9fead35d846ac8ce5f22380bfdd
xiamuceer <xiamuceer@gmail.com>
2025-11-28 16:21:51 +0800

    update

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,28 @@
 // .vitepress/theme/index.ts
 import DefaultTheme from 'vitepress/theme'
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
+import mediumZoom from 'medium-zoom'
+
 import './style.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  setup() {
+    const route = useRoute()
+    const initZoom = () => {
+      mediumZoom('.main img', {
+        background: 'var(--vp-c-bg)',
+        margin: 48,
+        scrollOffset: 0
+      })
+    }
+    onMounted(() => {
+      initZoom()
+    })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
+  },
+}
